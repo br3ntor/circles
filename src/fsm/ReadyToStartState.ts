63764408a2a4ab2d5e99a94ec6571883dc5417df
brent <br3ntor@gmail.com>
2025-07-18 23:59:55 -0700

    Fixed when barrier starts to be drawn.

--- src/fsm/ReadyToStartState.ts
+++ src/fsm/ReadyToStartState.ts
@@ -16,23 +16,6 @@ export class ReadyToStartState extends State {
     this.game.goal.draw(ctx);
     this.game.guardians.forEach((guardian) => guardian.draw(ctx));
 
-    // Draw left start area barrier
-    ctx.beginPath();
-    ctx.moveTo(100, 0);
-    ctx.lineTo(100, this.game.canvas.height);
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = "#DEDEDE";
-    ctx.stroke();
-    ctx.closePath();
-
-    // Draw startup message
-    ctx.fillStyle = "#DEDEDE";
-    ctx.font = "26px Arial";
-    ctx.textAlign = "center";
-    ctx.fillText(
-      "Click the white circle or press space bar to start the game",
-      this.game.canvas.width / 2,
-      this.game.canvas.height - 200
-    );
+    this.drawReadyUI(ctx);
   }
 }
